c2d39f3ff44dd66587dc0a5f7c985b7434704de2
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2025-03-06 17:19:17 +0600

    implement authentication methods in AuthProvider; add user state management and loading handling

--- src/Components/AuthProvider/AuthProvider.jsx
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -1,19 +1,55 @@
-import { createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase.config";
 
 export const authContext = createContext();
-const authProvider = ({children})=>{
-    const [user,setUser] = useState(null);
-    const [loading,setLoading] = useState(true);
-    const auth = getAuth(app)
-    
-    const createUser =(email,passwrod)=>{
-        
-    }
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const authInfo = {}
-    return(
-        <authContext.Provider value={authInfo}>
-            {children}
-        </authContext.Provider>
-    )
-}
+  const createUser = (email, password) => {
+    setLoading(true);
+   return createUserWithEmailAndPassword(auth, email, password);
+    
+  };
+
+  const Login = (email, password) => {
+    setLoading(true);
+   return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleProvider = new GoogleAuthProvider();
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const Logout = () => {
+    setLoading(true);
+   return signOut(auth);
+    
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authInfo = { Login, Logout, googleLogin, createUser, user, loading };
+  return (
+    <authContext.Provider value={authInfo}>{children}</authContext.Provider>
+  );
+};
+export default AuthProvider;
